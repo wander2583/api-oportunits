b4f9c69c33096b2753ee7aa27e33a3d37bdf073e
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-10-07 19:54:23 -0300

    feat: applying database creation.

--- handler/creatOpening.go
+++ handler/creatOpening.go
@@ -1,13 +1,15 @@
 package handler
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opengin",
-	})
+	request := struct {
+		role string
+	}{}
+
+	ctx.BindJSON(&request)
+
+	logger.Infof("request received: %+v", request)
 }
--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/wander2583/api-oportunits/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
