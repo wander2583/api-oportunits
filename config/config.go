343c7a25b5066540181dc79f8647fa477ff80398
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-10-07 15:07:09 -0300

    feat: updating the config

--- config/config.go
+++ config/config.go
@@ -3,9 +3,16 @@ package config
 import "gorm.io/gorm"
 
 var (
-	db *gorm.DB
+	db     *gorm.DB
+	logger *Logger
 )
 
 func Init() error {
 	return nil
+}
+
+func GetLogger() *Logger {
+	// Initialize Loger
+	logger = NewLogger(p)
+	return logger
 }
