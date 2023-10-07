8a9e88b0eac409d4815265b8e509bb2511ceb37e
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-10-07 12:22:55 -0300

    feat: Creating routes and implementing them in the general route.

--- router/router.go
+++ router/router.go
@@ -3,13 +3,12 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
- // Inicializa as rotas ultilizando as configuracoes defatlt do Gin
-	router *gin.Engine := gin.Default()
-  // Definindo a rota
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-  router.Run(addr...: ":8080") // listen and serve on 0.0.0.0:8080
+	// Inicializa as rotas ultilizando as configuracoes defatlt do Gin
+	router := gin.Default()
+
+	// Initialize Routes
+	initializeRoutes(router)
+
+	// Run the server
+	router.Run(":8080")
 }
