f01d454028af19b19b82fc20e1e306b77b46d740
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-10-07 12:41:55 -0300

    feat: creating the routes PUT DELETE LISTING GET ...

--- router/routes.go
+++ router/routes.go
@@ -14,5 +14,25 @@ func initializeRoutes(router *gin.Engine) {
 				"msg": "GET Opening",
 			})
 		})
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "POST Opening",
+			})
+		})
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "DELETE Opening",
+			})
+		})
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "PUT Opening",
+			})
+		})
+		v1.GET("/openings", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{
+				"msg": "LISTING Opening",
+			})
+		})
 	}
 }
