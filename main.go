60fd32c713c991bed204daddcf53d963035edc1e
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-10-07 14:16:09 -0300

    feat: placing the initialization inside main.

--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"github.com/wander2583/api-oportunits/config"
 	"github.com/wander2583/api-oportunits/router"
 )
 
 func main() {
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		panic(err)
+		return
+	}
+
+	// Initialize Router
 	router.Initialize()
 }
