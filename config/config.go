71aeddcf32b212445852bc0cad64e3ba7fba4489
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-10-07 16:32:33 -0300

    feat: configuring sqlite

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
@@ -10,8 +12,19 @@ var (
 )
 
 func Init() error {
-	// return errors.New("fake error")
+	var err error
+
+	db, err := InitializeSQLite()
+
+	if err != nil {
+		return fmt.Errorf("error intializing sqlite: %v", err)
+	}
+
 	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
 }
 
 func GetLogger(p string) *Logger {
