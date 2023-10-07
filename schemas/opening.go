eeb98ad7ac8b2fdf915d02cf1506348895b1a624
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-10-07 19:41:48 -0300

    feat: inserting the openingresponse

--- schemas/opening.go
+++ schemas/opening.go
@@ -1,6 +1,10 @@
 package schemas
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Opening struct {
 	gorm.Model
@@ -10,4 +14,17 @@ type Opening struct {
 	Remote   bool
 	Link     string
 	Salary   int64
+}
+
+type OpeningResponse struct {
+	ID       uint      `json:"id"`
+	CreateAt time.Time `json:"createAt"`
+	UpdateAt time.Time `json:"updateAt"`
+	DeleteAt time.Time `json:"deleteAt,omitempty"`
+	Role     string    `json:"role"`
+	Company  string    `json:"company"`
+	Location string    `json:"location"`
+	Remote   bool      `json:"remote"`
+	Link     string    `json:"link"`
+	Salary   int64     `json:"salary"`
 }
