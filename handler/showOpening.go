8b0eb73dc59886dc3baa26acba86cfaacbfd8c27
Wanderlei Pereira <wanderlei2583@gmail.com>
2023-10-08 09:35:14 -0300

    feat: entering the show settings.

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -4,10 +4,21 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/wander2583/api-oportunits/schemas"
 )
 
 func ShowOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"msg": "GET Opengin",
-	})
+	id := ctx.Query("id")
+
+	if id == "" {
+		sendError(ctx, http.StatusBadRequest, errParamIsRequire("id", "QueryParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+
+	sendSuccess(ctx, "show-opening", opening)
 }
